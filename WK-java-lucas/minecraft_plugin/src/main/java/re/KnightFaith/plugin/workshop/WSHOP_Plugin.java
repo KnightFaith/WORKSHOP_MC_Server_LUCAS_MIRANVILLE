8cd2770ccd7791a0581b5010cd0ef467fb104890
knightfaith <lucas.miranville@epitech.eu>
2023-02-03 15:50:35 +0400

    2nd Commit Class/Package

---
+++ WK-java-lucas/minecraft_plugin/src/main/java/re/KnightFaith/plugin/workshop/WSHOP_Plugin.java
@@ -0,0 +1,17 @@
+package re.KnightFaith.plugin.workshop;
+import org.bukkit.plugin.java;
+public class WSHOP_Plugin extends java.JavaPlugin {
+
+    onEnable() {
+        System.out.println("Sheit");
+    }
+
+    onDisable() {
+
+    }
+
+    org.bukkit.plugin.java.JavaPlugin.onEnable()
+    {
+        System.out.println("Sheit");
+    }
+}
